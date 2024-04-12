f714f267a56934ec144090e9da210515274d7a88
ShawnPan <shawnpan@ShawnPandeMacBook-Air.local>
2024-04-12 12:31:53 +0800

    test: 777

--- release.config.js
+++ release.config.js
@@ -1,15 +1,45 @@
 const config = {
-    branches: ['main'],
-    preset: "angular", // if we don't add this line, the release note will only display the commit message with feat, fix, BREAKING CHANGE.
-    plugins: [
-      '@semantic-release/commit-analyzer',
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    [
       '@semantic-release/release-notes-generator',
-      ["@semantic-release/git", {
-        "assets": ["dist/*.js", "dist/*.js.map"],
-        "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      }],
-      '@semantic-release/github'
-    ]
-  };
-  
-  module.exports = config;
+      {
+        preset: 'angular',
+        presetConfig: {
+          types: [
+            { type: 'docs', section: 'Docs' },
+            { type: 'refactor', section: 'Refactor' },
+            { type: 'test', section: 'Test' },
+          ],
+        },
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['dist/*.js', 'dist/*.js.map'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
+
+module.exports = config;
+
+
+// const config = {
+//   branches: ['main'],
+//   plugins: [
+//     '@semantic-release/commit-analyzer',
+//     '@semantic-release/release-notes-generator',
+//     ["@semantic-release/git", {
+//       "assets": ["dist/*.js", "dist/*.js.map"],
+//       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+//     }],
+//     '@semantic-release/github'
+//   ]
+// };
+
+// module.exports = config;
